61289a9b6fe975f041ed7c5716eb1aa6e39079d2
Khusamov Sukhrob <khusamov@yandex.ru>
2023-12-19 22:09:23 +0300

    #1 Добавлен ввод расстояния от верхнего края листа

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 import {saveAs} from 'file-saver'
-import {useState} from 'react'
+import {ChangeEvent, useState} from 'react'
 import useAsyncEffect from 'use-async-effect'
 import {PdfViewer} from './components/PdfViewer.tsx'
 import {createSamplePdf} from './functions/createSamplePdf.ts'
@@ -9,15 +9,16 @@ const pdfFilename = 'Типа это PDF.pdf'
 
 export const App = () => {
 	const [document, setDocument] = useState<Uint8Array | null>(null)
+	const [timelineTop, setTimelineTop] = useState<number>(50)
 
 	useAsyncEffect<null>(async isMounted => {
-		const pdfBytes = await createSamplePdf()
+		const pdfBytes = await createSamplePdf({timelineTop})
 		if (!isMounted()) {
 			return null
 		}
 		setDocument(pdfBytes)
 		return null
-	}, () => {}, [])
+	}, () => {}, [timelineTop])
 
 	const onDownloadButtonClick = () => {
 		if (document) {
@@ -25,9 +26,28 @@ export const App = () => {
 		}
 	}
 
+	const onTimelineTopInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const value = Number(event.target.value)
+		if (isNaN(value)) {
+			return
+		}
+		if (value > 100 || value < 0) {
+			return
+		}
+		setTimelineTop(value)
+	}
+
 	return (
 		<div>
-			<div>Timeline template creator</div>
+			<div>
+				Timeline template creator
+			</div>
+			<div>
+				<div>Расстояние от верхнего края листа до линии времени в процентах</div>
+				<div>
+					<input value={timelineTop} onChange={onTimelineTopInputChange}/>
+				</div>
+			</div>
 			<div>
 				<button disabled={document === null} onClick={onDownloadButtonClick}>
 					Скачать PDF
--- src/functions/createSamplePdf.ts
+++ src/functions/createSamplePdf.ts
@@ -1,13 +1,21 @@
 import {PageSizes, PDFDocument, rgb} from 'pdf-lib'
 import {landscape} from './landscape.ts'
 
-export async function createSamplePdf() {
+interface ICreateSamplePdfParams {
+	/**
+	 * Расстояние от верхнего края листа до линии времени в процентах.
+	 */
+	timelineTop?: number
+}
+
+export async function createSamplePdf({timelineTop = 50}: ICreateSamplePdfParams = {}) {
 	const pdfDoc = await PDFDocument.create()
 	const page = pdfDoc.addPage(landscape(PageSizes.A4))
 	const {width, height} = page.getSize()
+	const offsetRatioY = timelineTop / 100
 	page.drawLine({
-		start: {x: 0, y: height / 2},
-		end: {x: width, y: height / 2},
+		start: {x: 0, y: height * offsetRatioY},
+		end: {x: width, y: height * offsetRatioY},
 		thickness: 2,
 		color: rgb(0, 0, 0),
 		opacity: 1,
