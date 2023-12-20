31b2ac2cee4dd4af162071dcb56378a64d9f4272
Khusamov Sukhrob <khusamov@yandex.ru>
2023-12-20 23:03:10 +0300

    Добавлен вывод названия, даты сборки и версии программы

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import './index.scss'
+import {version, description} from '../package.json'
+
+console.log(`Программа: ${description}`)
+console.log(`Версия программы: ${version}`)
+console.log(`Дата сборки: ${BUILD_DATE}`)
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
